641e2e3126503655bce0fcdf06ce0951e55f4540
Ekaterina Moldavskaia <Ekaterina_Moldavskaia@epam.com>
2018-07-04 09:48:07 +0300

    Mail ru test with Selenium

--- src/test/java/MailRuTest.java
+++ src/test/java/MailRuTest.java
@@ -12,7 +12,6 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -27,7 +26,6 @@ public class MailRuTest {
         System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
     }
 
     @AfterClass(alwaysRun = true)
@@ -38,17 +36,23 @@ public class MailRuTest {
     @Test(description = "mail.ru page should open and contain appropriate title")
     public void loginTest() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
         // Open mail.ru page
         driver.navigate().to("https://mail.ru/");
         assertEquals(driver.getTitle(), "Mail.Ru: почта, поиск в интернете, новости, игры");
+
         // clear the email address field
         driver.findElement(By.name("login")).clear();
+
         //login to the mail box
         driver.findElement(By.name("login")).sendKeys("MoldavskaiaATM");
         driver.findElement(By.cssSelector(".input[type=password]")).sendKeys("ghbvtcm46");
         driver.findElement(By.cssSelector(".o-control[type=submit]")).click();
+
         // assert that the login was successful
         wait.until(ExpectedConditions.titleContains("Входящие - Почта Mail.Ru"));
+        assertEquals(driver.getTitle(), "Входящие - Почта Mail.Ru");
+
         //Create a new mail
         WebElement newMailButton = driver.findElement(By.linkText("Написать письмо"));
         assertTrue(newMailButton.isDisplayed());
@@ -67,12 +71,11 @@ public class MailRuTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[data-mnemo=\"saveStatus\"]")));
 
         //open drafts folder
-        //Thread.sleep(5000);
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-mnemo=\"drafts\"]"))).click();
-        //driver.findElement(By.cssSelector("[data-mnemo=\"drafts\"]")).click();
         assertEquals(driver.getTitle(), "Новое письмо - Почта Mail.Ru");
+
         // assert that draft presents in the Draft folder
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.titleIs("Черновики - Почта Mail.Ru"));
         List<WebElement> subjectsOfMailForTest = driver.findElements(By.cssSelector(".b-datalist__item__subj"));
         List<String> textOfSubjects = new ArrayList<String>();
         for (WebElement subject : subjectsOfMailForTest) {
@@ -80,26 +83,30 @@ public class MailRuTest {
             textOfSubjects.add(subject.getText().replace(bodyOfMail, "").trim());
         }
         assertTrue(textOfSubjects.contains(subjectToMail.toString()), "The draft of test email is absent in the folder");
-        //Open draft
+
+        //Open saved draft
         String toLocator = "[data-subject='" + subjectToMail + "']";
         driver.findElement(By.cssSelector(toLocator)).click();
         assertEquals(driver.getTitle(), "Новое письмо - Почта Mail.Ru");
+
         // assert that all field contain the same information that before saving as draft
         assertEquals(driver.findElement(By.cssSelector("[data-text='ekaterinamoldavskaia18@gmail.com']"))
                 .getText(), "ekaterinamoldavskaia18@gmail.com");
         JavascriptExecutor js = (JavascriptExecutor) driver;
         Object subToMail = js.executeScript("return document.getElementsByName('Subject')[0].value");
-
         assertEquals(subToMail, subjectToMail.toString());
         driver.switchTo().frame(driver.findElement(By.cssSelector("iframe")));
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#tinymce")));
         assertTrue(driver.findElement(By.cssSelector("#tinymce")).getText().contains("Text"), "Mail text is absent");
         driver.switchTo().defaultContent();
+
+        // send email
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-name='send']"))).click();
+
+        // assert that the draft disappeared from draft folder
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".message-sent__title")));
-        // assert that the draft disappears from draft folder
         driver.findElement(By.cssSelector("[data-mnemo='drafts']")).click();
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.titleIs("Черновики - Почта Mail.Ru"));
         List<WebElement> subjectsOfMailForTestAfterSending = driver.findElements(
                 By.cssSelector(".b-datalist__item__subj"));
         List<String> textOfSubjectsAfterSending = new ArrayList<String>();
@@ -112,9 +119,8 @@ public class MailRuTest {
                 subjectToMail.toString()), "The draft of test email is absent in the folder");
 
         //assert the sent mail presents in Sent folder
-        Thread.sleep(5000);
-        driver.findElement(By.cssSelector("[href='/messages/sent/']")).click();
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[href='/messages/sent/']"))).click();
+        wait.until(ExpectedConditions.titleIs("Отправленные - Почта Mail.Ru"));
         List<WebElement> sentMails = driver.findElements(By.cssSelector(".b-datalist__item__subj"));
         List<String> sentSubjects = new ArrayList<String>();
         for (WebElement sentMail : sentMails) {
@@ -122,5 +128,9 @@ public class MailRuTest {
             sentSubjects.add(sentMail.getText().replace(bodyOfMail, "").trim());
         }
         assertTrue(sentSubjects.contains(subjectToMail.toString()), "The sent test email is absent in the folder");
+
+        //Log off
+        driver.findElement(By.cssSelector("#PH_logoutLink")).click();
+        assertEquals(driver.getTitle(), "Mail.Ru: почта, поиск в интернете, новости, игры");
     }
 }
